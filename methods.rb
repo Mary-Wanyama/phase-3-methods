babf2cae6ba9db20061beb7390c022dd37ae056e
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-17 16:10:36 +0300

    completed assigment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+greet_programmer()
+
+def greet name 
+    puts "Hello, #{name}!"
+    "Hello, #{name}!"
+end 
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+    "Hello, #{name}!"
+end
+greet_with_default("Naureen")
+def add num1, num2
+    puts num1 
+    puts num2
+   return num1 + num2
+end
+add(1, 2)
+
+def halve num
+    if num.class != Integer
+        return nil
+    end
+    puts num/2
+    num/2
+end
+halve(4)
+halve(5)
+halve('ruby')
